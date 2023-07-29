ed8d4290786742b2ead523a573b8c8ee6f2d6d74
Sampurn Chouksey <sampurn10chouksey@gmail.com>
2023-07-30 01:54:19 +0530

    Remove unused import of 'package:flutter/widgets.dart'.

--- lib/widgets/chats_message.dart
+++ lib/widgets/chats_message.dart
@@ -1,9 +1,7 @@
-import 'package:chat_app/screens/chat.dart';
 import 'package:chat_app/widgets/message_buble.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class ChatMessages extends StatelessWidget {
   const ChatMessages({super.key});
