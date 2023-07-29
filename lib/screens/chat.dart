90d09ad2173fa91458e3b0e946bed4f55a1fc6a2
Sampurn Chouksey <sampurn10chouksey@gmail.com>
2023-07-30 02:21:58 +0530

    Added Firebase Messaging to the ChatScreen. Added a method to initialize Firebase Messaging and subscribe to the chat topic.

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,16 +1,35 @@
 import 'package:chat_app/widgets/chats_message.dart';
 import 'package:chat_app/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void SetupMethod() async {
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission();
+    fcm.subscribeToTopic('chat');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    final fcm = FirebaseMessaging.instance;
+    SetupMethod();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Owl Chats'),
+          title: const Text('Owl Chats'),
           actions: [
             IconButton(
               onPressed: () {
